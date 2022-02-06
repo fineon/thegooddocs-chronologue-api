ab3d441b62fa06a28bed1880fa402baab9f3fb8a
Ian Nguyen <ian@heythereian.com>
2022-02-05 21:45:15 -0800

    added more information and events in api data, with a country field

--- api-data.js
+++ api-data.js
@@ -20,7 +20,7 @@ const formatDateToISOString = (date) => {
 };
 
 const formatUTCDatetoLocale = (date) => {
-  let sentIsoDate = date.replace(/-/g, '\/').replace(/T.+/, '');
+  let sentIsoDate = date.replace(/-/g, "/").replace(/T.+/, "");
   let toLocaleDateString = new Date(sentIsoDate).toLocaleDateString();
   console.log(toLocaleDateString);
   return toLocaleDateString;
@@ -28,24 +28,24 @@ const formatUTCDatetoLocale = (date) => {
 
 // prevents date conversion from moving one day backward
 
-
 // sorted from past to future
 let experimentalData = [
   {
-    year: '1968',
+    year: "1781",
     allContinents: [
       {
-        continent: 'north-america',
-        country: 'USA',
+        continent: "europe",
+        country: "null",
         events: [
           {
-            event: 'The Orbiting Astronomical Observatory 2 was the first space telescope launched',
-            date: formatDateToISOString('1968-12-07'),
-            image: 'http://www.sal.wisc.edu/OAO/oao1reg.jpg',
-            video: '',
+            event: "Sir William Herschel discovered Uranus",
+            date: formatDateToISOString("1781-03-13"),
+            image:
+              "https://solarsystem.nasa.gov/system/feature_items/images/88_carousel_uranus.jpg",
+            video: "https://www.youtube.com/watch?v=hx9VFtqiNM4",
             links: [
-                'https://en.wikipedia.org/wiki/Space_exploration#Telescope',
-                'http://www.sal.wisc.edu/OAO/'
+              "https://en.wikipedia.org/wiki/Uranus",
+              "https://solarsystem.nasa.gov/planets/uranus/overview/",
             ],
           },
         ],
@@ -53,105 +53,179 @@ let experimentalData = [
     ],
   },
   {
-    year: '1957',
+    year: "1968",
     allContinents: [
       {
-        continent: 'europe',
-        country: 'Soviet',
+        continent: "north-america",
+        country: "USA",
         events: [
           {
-            event: 'The first successful orbital launch of the Soviet uncrewed Sputnik 1 mission',
-            date: formatDateToISOString('1957-10-04'),
-            image: 'https://upload.wikimedia.org/wikipedia/commons/b/be/Sputnik_asm.jpg',
-            video: 'link',
+            event:
+              "The Orbiting Astronomical Observatory 2 was the first space telescope launched",
+            date: formatDateToISOString("1968-12-07"),
+            image: "http://www.sal.wisc.edu/OAO/oao1reg.jpg",
+            video: "",
             links: [
-                'https://en.wikipedia.org/wiki/Space_exploration#Telescope',
-                'https://en.wikipedia.org/wiki/Sputnik_1',
-            ]
+              "https://en.wikipedia.org/wiki/Space_exploration#Telescope",
+              "http://www.sal.wisc.edu/OAO/",
+            ],
           },
         ],
       },
     ],
   },
   {
-    year: '1961',
+    year: "1957",
     allContinents: [
       {
-        continent: 'europe',
-        country: 'Russia',
+        continent: "europe",
+        country: "Soviet",
         events: [
           {
-            event: 'The first successful human spaceflight was Vostok 1, carrying the 27-year-old Russian cosmonaut, Yuri Gagarin',
-            date: formatDateToISOString('1961-04-12'),
-            image: 'https://upload.wikimedia.org/wikipedia/commons/d/df/Vostok_spacecraft.jpg',
-            video: 'link',
+            event:
+              "The first successful orbital launch of the Soviet uncrewed Sputnik 1 mission",
+            date: formatDateToISOString("1957-10-04"),
+            image:
+              "https://upload.wikimedia.org/wikipedia/commons/b/be/Sputnik_asm.jpg",
+            video: "link",
             links: [
-                'https://en.wikipedia.org/wiki/Space_exploration#Telescope',
-                'https://en.wikipedia.org/wiki/Vostok_1'
-            ]
+              "https://en.wikipedia.org/wiki/Space_exploration#Telescope",
+              "https://en.wikipedia.org/wiki/Sputnik_1",
+            ],
           },
         ],
       },
     ],
   },
   {
-    year: '2012',
+    year: "1961",
     allContinents: [
       {
-        continent: 'north-america',
-        country: 'USA',
+        continent: "europe",
+        country: "Russia",
         events: [
           {
-            event: 'Voyager 1 became the first human-made object to leave the Solar System into interstellar space',
-            date: formatDateToISOString('2012-08-25'),
-            image: 'https://upload.wikimedia.org/wikipedia/commons/6/60/Voyager_spacecraft_model.png',
-            video: 'link',
+            event:
+              "The first successful human spaceflight was Vostok 1, carrying the 27-year-old Russian cosmonaut, Yuri Gagarin",
+            date: formatDateToISOString("1961-04-12"),
+            image:
+              "https://upload.wikimedia.org/wikipedia/commons/d/df/Vostok_spacecraft.jpg",
+            video: "link",
             links: [
-                'https://en.wikipedia.org/wiki/Space_exploration#Telescope',
-                'https://en.wikipedia.org/wiki/Voyager_1'
-            ]
+              "https://en.wikipedia.org/wiki/Space_exploration#Telescope",
+              "https://en.wikipedia.org/wiki/Vostok_1",
+            ],
           },
         ],
       },
     ],
   },
   {
-    year: '2021',
+    year: "2012",
     allContinents: [
       {
-        continent: 'north-america',
-        country: 'USA',
+        continent: "north-america",
+        country: "USA",
         events: [
           {
-            event: 'NASA Perseverance rover landed on Mars to find clues on past microbial life',
-            date: formatDateToISOString('2021-02-18'),
-            image: 'https://www.nasa.gov/sites/default/files/thumbnails/image/09_touchdown_10k.jpg',
-            video: 'link',
+            event:
+              "Voyager 1 became the first human-made object to leave the Solar System into interstellar space",
+            date: formatDateToISOString("2012-08-25"),
+            image:
+              "https://upload.wikimedia.org/wikipedia/commons/6/60/Voyager_spacecraft_model.png",
+            video: "link",
             links: [
-                'https://www.nasa.gov/press-release/nasa-invites-public-to-share-thrill-of-mars-perseverance-rover-landing',
-                ''
-            ]
+              "https://en.wikipedia.org/wiki/Space_exploration#Telescope",
+              "https://en.wikipedia.org/wiki/Voyager_1",
+            ],
           },
         ],
       },
     ],
   },
   {
-    year: '2024',
+    year: "2016",
     allContinents: [
       {
-        continent: 'north-america',
-        country: 'USA',
+        continent: "australia",
+        country: "australia",
         events: [
           {
-            event: 'NASA Artemis mission sending the first woman and man to the Moon by 2024',
-            date: formatDateToISOString('2024'),
-            image: 'https://blogs.nasa.gov/artemis/wp-content/uploads/sites/303/2021/10/OrionStacked.jpg',
-            video: 'link',
+            event:
+              "European Southern Observatory (ESO) find Proxima b in habitable zone around Proxima Centauri. The long-sought world orbits its cool red parent star every 11 days and has a temperature suitable for liquid water to exist on its surface. ",
+            date: formatDateToISOString("2016"),
+            image: "https://cdn.eso.org/images/newsfeature/eso1629a.jpg",
+            video: "link",
+            links: ["https://www.eso.org/public/news/eso1629/"],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    year: "2021",
+    allContinents: [
+      {
+        continent: "north-america",
+        country: "USA",
+        events: [
+          {
+            event:
+              "NASA Perseverance rover landed on Mars to find clues on past microbial life",
+            date: formatDateToISOString("2021-02-18"),
+            image:
+              "https://www.nasa.gov/sites/default/files/thumbnails/image/09_touchdown_10k.jpg",
+            video: "link",
             links: [
-                'https://www.nasa.gov/specials/artemis/',
-                'https://blogs.nasa.gov/artemis/2021/10/20/stacking-operations-for-artemis-i-mission-nearing-completion/'
-            ]
+              "https://www.nasa.gov/press-release/nasa-invites-public-to-share-thrill-of-mars-perseverance-rover-landing",
+              "",
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    year: "2024",
+    allContinents: [
+      {
+        continent: "north-america",
+        country: "USA",
+        events: [
+          {
+            event:
+              "NASA Artemis mission sending the first woman and man to the Moon by 2024",
+            date: formatDateToISOString("2024"),
+            image:
+              "https://blogs.nasa.gov/artemis/wp-content/uploads/sites/303/2021/10/OrionStacked.jpg",
+            video: "link",
+            links: [
+              "https://www.nasa.gov/specials/artemis/",
+              "https://blogs.nasa.gov/artemis/2021/10/20/stacking-operations-for-artemis-i-mission-nearing-completion/",
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    year: "3000",
+    allContinents: [
+      {
+        continent: "north-america",
+        country: "USA",
+        events: [
+          {
+            event:
+              "Light pollution in Europe is so bad, that you can not see stars with the naked eye any more.",
+            date: formatDateToISOString("3000"),
+            image:
+              "https://www.darksky.org/wp-content/uploads/2014/09/Light_Pollution_Diagram_680px.jpg",
+            video: "https://www.youtube.com/watch?v=isoJ0XIKDlc",
+            links: [
+             'https://www.darksky.org/light-pollution/',
+             'https://blue-marble.de/nightlights/2012'
+            ],
           },
         ],
       },
@@ -163,32 +237,28 @@ let experimentalData = [
 // SHOULD BE A SOURCE OF TRUTH FOR ALL DATA AND API CALLS
 exports.allYearsAllLocation = [...experimentalData];
 
-
 // GET all location data
 exports.allLocationsID = [
   {
     continent: [
-      'asia',
-      'africa',
-      'europe',
-      'north-america',
-      'south-america',
-      'oceania',
-      'antartica',
+      "asia",
+      "africa",
+      "europe",
+      "north-america",
+      "south-america",
+      "oceania",
+      "antartica",
     ],
-    country: [
-      'USA',
-      'Soviet',
-      'Russia',
-    ],
+    country: ["USA", "Soviet", "Russia"],
     metadata: [
       {
         listLength: 7,
       },
       {
-        source: 'https://en.wikipedia.org/wiki/List_of_continents_and_continental_subregions_by_population',
-        liscene: 'CC BY-SA 3.0',
-        link: 'https://en.wikipedia.org/wiki/Wikipedia:Text_of_Creative_Commons_Attribution-ShareAlike_3.0_Unported_License',
+        source:
+          "https://en.wikipedia.org/wiki/List_of_continents_and_continental_subregions_by_population",
+        liscene: "CC BY-SA 3.0",
+        link: "https://en.wikipedia.org/wiki/Wikipedia:Text_of_Creative_Commons_Attribution-ShareAlike_3.0_Unported_License",
       },
     ],
   },
@@ -201,8 +271,7 @@ exports.allYears = [
 
 // GET all available API routes
 exports.allRoutes = [
-  {locations: '/all-locations-id'},
-  {years: '/all-years-id'},
-  {locationAndYears: '/year/:year-id/location/:location-id'},
+  { locations: "/all-locations-id" },
+  { years: "/all-years-id" },
+  { locationAndYears: "/year/:year-id/location/:location-id" },
 ];
-
